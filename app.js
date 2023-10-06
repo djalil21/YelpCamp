9ab33bbf42f529ed7a07ed74ca5a1ad58dd2c005
djalil21 <abdeldjalilbouzenzanaa@gmail.com>
2023-10-06 21:15:12 +0100

    add start script

--- app.js
+++ app.js
@@ -149,7 +149,7 @@ app.use((err, req, res, next) => {
     //req.flash('error', err.message)
     //res.status(statusCode).redirect("/");
 })
-
-app.listen(3000, () => {
-    console.log("serving on port 3000");
+const port =process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`serving on port ${port}`);
 });
